7284f17b6f72fd078f4ca5cefb2195913c50c1df
phipsoncheukyil <cheukyil@andrew.cmu.edu>
2020-10-30 18:16:10 -0400

    Updated home page links

--- homework_6/src/components/Home.js
+++ homework_6/src/components/Home.js
@@ -15,8 +15,8 @@ class HomePage extends React.Component {
                     <h2 className="BBIntroTitle">Welcome to the Bun Bun Bakery eStore!</h2>
                     <p className="BBIntroDesc">To get started, go to the BUNS page and search through our buns. Choose the buns you desire, customize it with any glaze of your choice, and add the buns to your cart. Once you're done, you can go to your cart at checkout, purchase your buns, sit back and relax as we prepare your order within 20 minutes.</p>
                     <div className="BBIntroLinkWrapper">
-                        <a href="products.html" className="BBIntroLink" >What buns are there?</a> 
-                        <a href="about.html" className="BBIntroLink" >What is Bun Bun Bakery?</a>
+                        <a href="#" className="BBIntroLink" onClick={() => this.props.setSite(1)}>What buns are there?</a> 
+                        <a href="#" className="BBIntroLink" onClick={() => this.props.setSite(2)}>What is Bun Bun Bakery?</a>
                     </div>
                 </div>
             </div>
